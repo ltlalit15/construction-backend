3fa5b39577c3453b125ca2596dc2c828ff3f2cba
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-10 18:54:27 +0530

    Update formResponseRouter.js

--- backend/Router/formResponseRouter.js
+++ backend/Router/formResponseRouter.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const {  } = require('../Controller/formResponseController');
+const { createFormResponse, getAllFormResponses, getFormResponseById, updateFormResponse, deleteFormResponse } = require('../Controller/formResponseController');
 
 const router = express.Router();
+
+router.post('/',createFormResponse);
+
+router.get('/',getAllFormResponses)
+
+router.get('/:id',getFormResponseById)
+
+router.delete('/:id',deleteFormResponse)
+
+router.patch('/:id',updateFormResponse)
 
 
 module.exports = router;
