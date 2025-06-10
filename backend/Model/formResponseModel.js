a11c2880369de4c05c7532e3667c11b6bf40f0a4
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-10 18:53:56 +0530

    Update formResponseModel.js

--- backend/Model/formResponseModel.js
+++ backend/Model/formResponseModel.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const formResponseSchema = new mongoose.Schema({
+  formName: { type: String, required: true },
+  submittedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },  // Reference to User
+ responses: { type: Map, required: true},
+  image: [String], // Array of image URLs
+}, { timestamps: true });
+
+
+module.exports = mongoose.model("FormResponse", formResponseSchema);
