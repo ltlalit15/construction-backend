e84dc985d2588d0775e185f12147b81520e20713
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-10 12:52:49 +0530

    Update formTemplateModel.js

--- backend/Model/formTemplateModel.js
+++ backend/Model/formTemplateModel.js
@@ -5,6 +5,11 @@ const FieldSchema = new mongoose.Schema({
   label: { type: String, required: true },               
   name: { type: String, required: true },
   type: { type: String, required: true },
+   createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   required: { type: Boolean },
   placeholder: { type: String },
   defaultValue: { type: mongoose.Schema.Types.Mixed },
