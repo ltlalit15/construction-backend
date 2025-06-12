3c51e78991cf94360d6a9d00dd2c9b94fc6b40e7
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-12 11:48:33 +0530

    Update IncidentModel.js

--- backend/Model/IncidentModel.js
+++ backend/Model/IncidentModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const incidentSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   incidentType: { type: String, required: true },  // Type of incident (e.g., Accident, Fire, etc.)
   dateTime: { type: Date, required: true },  // Date and time of the incident
   location: { type: String, required: true },  // Incident location
